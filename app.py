b24f79af1024145a235e900d8a07e0f8d1e29a05
cmjimerson1s <cmjimerson1s@gmail.com>
2023-08-09 18:26:53 +0200

    Add notes for function in app.py

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 import streamlit as st 
 from app_pages.multipage import MultiPage
 
+#Import from each page the functions for displaying dashboard page
 from app_pages.page_one_summary import page_one_body
 from app_pages.page_two_visualizer import page_two_body
 from app_pages.page_three_drowsy_detector import page_three_body
 from app_pages.page_four_hypothesis import page_four_body
 from app_pages.page_five_ml_performance import page_five_body
 
-
 app = MultiPage(app_name= "Drowsiness Detector")
-
 
 app.add_page("Project Summary", page_one_body)
 app.add_page("Drowsiness Visualization", page_two_body)
